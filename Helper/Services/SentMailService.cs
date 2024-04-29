3b6025a620997956960b6f534ac602477d4181a8
ngochung130201 <hung.ngochuynh@infodation.vn>
2024-04-29 15:18:15 +0700

    sdsa

--- Helper/Services/SentMailService.cs
+++ Helper/Services/SentMailService.cs
@@ -19,7 +19,7 @@ namespace Helper.Services
                 {
                     response.Message = "Mail sent successfully";
                     response.Status = true;
-
+                    // Test
                 }
                 else
                 {
